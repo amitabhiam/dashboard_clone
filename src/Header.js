bcaef26a23a94c99d964c5c720252b318bebc084
amitabhiam <amitabhs132@gmail.com>
2024-01-20 19:05:07 +0530

    changes

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <div>
+        <div className="text-sm font-medium text-blue-900">This is Header </div>
+    </div>
+    )
+}
+
+export default Header;
