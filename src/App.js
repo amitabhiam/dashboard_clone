37648a2d0019d30c1fb1b384a5ff815b167209bb
amitabhiam <amitabhs132@gmail.com>
2024-01-23 15:19:10 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from './Header';
 
 function App() {
   return (
-    <div className="border-green-900 border-b p-1 m-1 rounded">
-      <div className="text-3xl font-medium text-blue-900 ">This is Body </div>
+    <div >
+      <div> </div>
       <Header />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Header = () => {
 
-    const API = "https://hub.dummyapis.com/employee?";
-
-    // using axios to fetch data
-    const [value, setValue] = useState([]);
-
-    const fetchData = async (url) => {
-        try {
-            const res = await fetch(url);
-            const data = await res.json(url);
-            setValue(data);
-            console.log(data);
-        }
-
-        catch(error) {
-            console.log(error);
-        }
-    }
-
-    useEffect(() => {
-        fetchData(API);
-    }, []);
 
     return (
     <div>
-        <div className="text-sm font-medium text-blue-900">This is Header </div>
-        {value.map((item) => {
-            return <div className="grid gap-4 grid-cols-3 grid-rows-3 bg-orange-300">{item.firstName}</div>
-        })}
+        <img src='/images/dashboard_icon.png' alt='icon' />
+        <input type='text'>Search</input>
     </div>
     )
 }
