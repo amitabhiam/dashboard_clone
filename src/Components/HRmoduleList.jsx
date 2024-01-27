53708fc865d82e9f83b4fb1b02e7a663d14c6a06
Abhishek Bhardwaj <eduandabhi@gmail.com>
2024-01-27 18:32:31 +0530

    Naviagte to taxkbox page added

--- src/Components/HRmoduleList.jsx
+++ src/Components/HRmoduleList.jsx
@@ -1,50 +1,39 @@
-import React from 'react';
-import HRMmodule from './HRMmodule';
-import { useNavigate } from 'react-router';
-import { Link } from 'react-router-dom';
-import TaskBox from '../TaskBox';
+import React from "react";
+import HRMmodule from "./HRMmodule";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import TaskBox from "../TaskBox";
 
-function HRmoduleList({moduleList}) {
-    // const navigate=useNavigate();
+function HRmoduleList({ moduleList }) {
+  const navigate = useNavigate();
 
-    // const handleModuleClick = (moduleCode) => {
-    //     switch(moduleCode){
-    //         case 'TASK_BOX':
-    //             navigate('/taskbox')
-    //             break;
-    //          case 'ATTENDANCE':
+  const handleModuleClick = (moduleCode) => {
+    switch (moduleCode) {
+      case "TASK_BOX":
+        navigate("/taskbox");
+        break;
+      case "ATTENDANCE":
+        break;
 
-    //          break;
-
-    //         default:{
-
-    //         }
-    //     }
-    // }
-
-    const handleModuleClick = (moduleCode) => {
-        console.log(moduleCode);
+      default: {
+      }
     }
+  };
 
-    <Link 
-    to='/taskbox'
-    onClick={handleModuleClick}
-    >
-        <TaskBox />
-    </Link>
-   
-    
-
-    return (
-    <div className='flex flex-wrap gap-4'>
-        <div className='flex flex-wrap w-full gap-[64px]'>
-            
-            {moduleList.map((item) => {
-              return <div> <HRMmodule hrModule={item} onClickModule={handleModuleClick} /></div>
-            })}
-        </div>
+  return (
+    <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap w-full gap-[64px]">
+        {moduleList.map((item) => {
+          return (
+            <div>
+              {" "}
+              <HRMmodule hrModule={item} onClickModule={handleModuleClick} />
+            </div>
+          );
+        })}
+      </div>
     </div>
-    )
+  );
 }
 
 export default HRmoduleList;
