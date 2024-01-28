d64480a8f5bc5906a260f7324ac0fec1bb06cf93
amitabhiam <amitabhs132@gmail.com>
2024-01-28 17:22:24 +0530

    updates

--- src/Components/HRMmodule.jsx
+++ src/Components/HRMmodule.jsx
@@ -5,20 +5,21 @@ function HRMmodule({hrModule, onClickModule}) {
     const {url, title, moduleCode} = hrModule;
     return (
     <div>
-    <div className=''>
-        <div className='flex flex-col items-center justify-center shadow-md w-[130px] h-[130px] bg-[#4C8F93] rounded-lg' onClick={() => onClickModule(moduleCode)}>
         <div>
-            <img className='w-[100px] h-[100px]' src={url} alt='Notes' />
-        </div>
-        </div>
+            <div className='flex flex-col items-center justify-center shadow-md w-[130px] h-[130px] bg-[#4C8F93] rounded-lg' onClick={() => onClickModule(moduleCode)} >
+                <div>
+                    <img className='w-[100px] h-[100px]' src={url} alt='Notes' />
+                </div>
+            </div>
 
-        <div className='text-[#000] text-[20px]  font-normal text-center'>
-            {title}
-        </div>
-        
+            <div className='text-[#000] text-[20px]  font-normal text-center'>
+                {title}
+            </div>
         </div>
     </div>
     )
 }
 
 export default HRMmodule;
+
+// onClick={() => onClickModule(moduleCode)}
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Search } from '@mui/icons-material';
 import { NotificationAddRounded } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-
     return (
     <div className='fixed inline-flex justify-evenly top-0 w-full bg-white'>
-        <img src='/images/dashboard_icon.png' alt='icon' />
+        <Link to='/'>
+            <img src='/images/dashboard_icon.png' alt='icon' />
+        </Link>
         <div className='flex justify-evenly ml-auto  border-gray-500'>
             <input className='h-8 w-100%' type='text' placeholder='search...'></input>
             <Search className='h-22'/>
