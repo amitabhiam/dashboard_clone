32c776a86f0acf0785a90cd75f86997e93ff89b1
amitabhiam <amitabhs132@gmail.com>
2024-01-29 22:16:52 +0530

    merging header

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import { Search } from '@mui/icons-material';
-import { NotificationAddRounded } from '@mui/icons-material';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Search } from "@mui/icons-material";
+import { NotificationAddRounded } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 import { Apps } from "@mui/icons-material";
-import SideBar from './SideBarComponents/SideBar';
-import { useState } from 'react';
+import NotificationList from "./NotificationList";
 
 const Header = () => {
-    
-    const [menu, setMenu] = useState(false)
+  const [noti, setNoti] = useState(false);
 
-    const handleClick = () => {
-        setMenu(!menu);
-    }
-    
+  const handleNotification = () => {
+    setNoti(!noti);
+  };
 
-    return (
-    <div className='fixed flex justify-evenly top-0 w-full h-[70px] bg-[#FFFFFF]'>
-        <div className="inner-sidebar relative ml-2"  >
-            <Apps onClick={handleClick}/>
-                {menu ? <SideBar/> : null}
-        </div>
-        <div className='ml-6'>
-        <Link to='/'>
-            <img src='/images/dashboard_icon.png' alt='icon' />
+  return (
+    <div className="fixed flex justify-evenly top-0 w-full h-[70px] bg-red-500">
+      <div className="ml-2">
+        <Apps />
+      </div>
+      <div className="ml-6">
+        <Link to="/">
+          <img src="/images/dashboard_icon.png" alt="icon" />
         </Link>
-        </div>
-        <div className='ml-auto justify-center  border-gray-500'>
-            <input className='h-[25px] w-100%' type='text' placeholder='search...'></input>
-            <Search className='h-[28px]'/>
-        </div>
-        <div className='ml-auto mr-6'>
-            <NotificationAddRounded className='' />
-        </div>
+      </div>
+      <div className="ml-auto justify-center  sborder-gray-500">
+        <input
+          className="h-[25px] w-100%"
+          type="text"
+          placeholder="search..."
+        ></input>
+        <Search className="h-[28px]" />
+      </div>
+      <div className="relative ml-auto mr-6">
+        <NotificationAddRounded onClick={handleNotification} />
+        {noti && <NotificationList />}
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Header;
